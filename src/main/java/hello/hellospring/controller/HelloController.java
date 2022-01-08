8fc90bb4d1534d87666ad18b9c08d3914cedb955
m04j00 <m04j00@gmail.com>
2022-01-09 00:07:53 +0900

    #3 파라미터를 받아 사용하는 페이지 구현

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
         return "hello";
     }
     @GetMapping("hello-mvc")
-    public String helloMvc(@RequestParam("name") String name, Model model){
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
     }
